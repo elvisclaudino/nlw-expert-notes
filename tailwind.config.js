35bd541d4318e5f6e3cf2163fc8073fe38577d1a
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2024-02-10 14:44:34 -0300

    starting project

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.tsx"],
+  theme: {
+    extend: {
+      colors: {
+        rocketseat: "#8257e6",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
